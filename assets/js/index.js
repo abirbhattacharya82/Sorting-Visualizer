44e1e8e4178191b80632f023e5f3c46828ea8f39
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-07-11 16:41:12 +0530

    Selection Sort

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
-var a=[80, 72, 53, 86, 98, 88, 52, 69, 73, 85, 95, 62, 76, 57, 59, 81, 88, 64, 53, 69, 90, 79, 98, 60, 50, 73, 98, 92, 59, 79, 58, 92, 57, 82, 73, 64, 65, 58, 62, 83, 98, 85, 65, 56, 84, 61, 75, 97, 71, 56];
+var a=[80, 72, 53, 86, 25, 22, 52, 69, 30, 26, 95, 62, 76, 57, 59, 81, 88, 37, 53, 40, 90, 79, 99, 60, 50, 73, 98, 23, 59, 21, 58, 92, 57, 82, 35, 64, 45, 58, 62, 83, 20, 85, 65, 56, 84, 61, 75, 97, 71, 56];
 function reset()
 {
     var n=a.length;
-    var b=[80, 72, 53, 86, 98, 88, 52, 69, 73, 85, 95, 62, 76, 57, 59, 81, 88, 64, 53, 69, 90, 79, 98, 60, 50, 73, 98, 92, 59, 79, 58, 92, 57, 82, 73, 64, 65, 58, 62, 83, 98, 85, 65, 56, 84, 61, 75, 97, 71, 56];
+    var b=[80, 72, 53, 86, 25, 22, 52, 69, 30, 26, 95, 62, 76, 57, 59, 81, 88, 37, 53, 40, 90, 79, 99, 60, 50, 73, 98, 23, 59, 21, 58, 92, 57, 82, 35, 64, 45, 58, 62, 83, 20, 85, 65, 56, 84, 61, 75, 97, 71, 56];
     for(var i=0;i<n;i++)
     {
         var index=""+i;
@@ -26,7 +26,6 @@ async function bubble_sort()
                 var t=a[j];
                 a[j]=a[j+1];
                 a[j+1]=t;
-
                 await sleep(1)
                 {
                     var x=""+j;
@@ -35,6 +34,31 @@ async function bubble_sort()
                     document.getElementById(y).style.width=""+a[j+1]+"%";
                 }
             }
+        }
+    }
+    console.log(a);
+}
+async function selection_sort()
+{
+    var n=a.length;
+    var i, j, min_idx;
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        if (a[j] < a[min_idx])
+        {
+            min_idx = j;
+        }
+        var t=a[i];
+        a[i]=a[min_idx];
+        a[min_idx]=t;
+        await sleep(1)
+        {
+            var x=""+i;
+            document.getElementById(x).style.width=""+a[i]+"%";
+            var y=""+(min_idx);
+            document.getElementById(y).style.width=""+a[min_idx]+"%";
         }
     }
     console.log(a);
